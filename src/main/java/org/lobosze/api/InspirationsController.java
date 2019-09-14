f89fa3d6cb0e762b01020e44a74eec4ae6df0a83
piotr <piotr.guziorski@gmail.com>
2019-09-14 23:21:53 +0200

    fd

--- src/main/java/org/lobosze/api/InspirationsController.java
+++ src/main/java/org/lobosze/api/InspirationsController.java
@@ -1,15 +1,15 @@
 package org.lobosze.api;
 
-import org.lobosze.api.data.City;
+import com.jayway.jsonpath.DocumentContext;
+import com.jayway.jsonpath.JsonPath;
 import org.lobosze.api.data.CityInfo;
-import org.lobosze.api.data.Country;
 import org.lobosze.api.data.Tile;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
-import java.util.ArrayList;
+import java.io.InputStream;
 import java.util.List;
 
 @RestController
@@ -20,18 +20,6 @@ public class InspirationsController {
     public List<Tile> getTiles() throws IOException {
         ObjectMapper mapper = new ObjectMapper();
         return mapper.readValue(getClass().getResourceAsStream("data/tiles.json"), new TypeReference<List<Tile>>(){});
-    }
-
-    @GetMapping("/cities")
-    public List<String> getCities() throws IOException {
-        List<String> ret = new ArrayList<>();
-        for(Tile tile: getTiles()) {
-            for(Country country:tile.getCountries()) {
-                for (City city:country.getCities())
-                    ret.add(city.getName());
-            }
-        }
-        return ret;
     }
 
     @GetMapping("/city")
--- src/test/java/org/lobosze/LotApiTest.java
+++ src/test/java/org/lobosze/LotApiTest.java
@@ -1,43 +1,21 @@
 package org.lobosze;
 
+import com.jayway.jsonpath.DocumentContext;
+import com.jayway.jsonpath.JsonPath;
 import okhttp3.*;
 import org.lobosze.api.lot.LotApiClient;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.Map;
 
 @Test
 public class LotApiTest {
 
     public void doTell() throws IOException {
-        LotApiClient client = new LotApiClient("PL", "EN");
 
-        String token = client.getToken();
-        Map connections = client.getConnections(token, "WAW", new String[]{"CDG"}, "22102019", "25112019", 1);
-
-        token.toString();
-        connections.toString();
-
-
-
-
-//        RequestBody body = RequestBody.create(MediaType.parse("application/json"), "{ \"params\": { \"cabinClass\": \"E\", \"market\": \"PL\", \"departureDate\": [\"22022019\"], \"returnDate\": \"25022019\", \"origin\": [\"WAW\"], \"tripType\": \"R\", \"adt\": 1, \"destination\": [\"CDG\"] }, \"options\": {\"fromCache\": true,\"fareType\": [\"ALL\"]}}\n");
-//
-//        Request request = new Request.Builder()
-//                .url("https://api.lot.com/flights-dev/v2/booking/availability")
-//                .post(body)
-//                .addHeader("X-Api-Key", LotApiClient.X_API_KEY)
-//                .addHeader("Authorization", "Bearer " + token)
-//                .build();
-//
-//        OkHttpClient xclient = new OkHttpClient();
-//
-//        Call call = xclient.newCall(request);
-//        Response response = call.execute();
-//        String string = response.body().string();
-//        Assert.assertEquals(response.code(),200);
 
     }
 }
