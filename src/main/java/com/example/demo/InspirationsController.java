15ee2d50a2a9cba4e2f7a28eccd7da0fbe4f77bd
piotr <piotr.guziorski@gmail.com>
2019-09-14 18:23:17 +0200

    mocked data

--- src/main/java/com/example/demo/InspirationsController.java
+++ src/main/java/com/example/demo/InspirationsController.java
@@ -1,10 +1,23 @@
 package com.example.demo;
 
+import com.example.demo.demo.Tile;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
+import java.util.List;
+
 @RestController
 public class InspirationsController {
+
+    @RequestMapping("/tiles")
+    public List<Tile> getTiles() throws IOException {
+        ObjectMapper mapper = new ObjectMapper();
+        return mapper.readValue(getClass().getResourceAsStream("data/tiles.json"), new TypeReference<List<Tile>>(){});
+    }
+
     @RequestMapping("/ping")
     public boolean ping() {
         return true;
